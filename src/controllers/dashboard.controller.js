8fe50464429fd70fecc2a8f4940027eb9482627b
tilak-dev <tsingh99991@gmail.com>
2024-10-28 15:45:24 +0530

    workiing with chnnel get stats

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -13,32 +13,65 @@ const getChannelStats = asyncHandler(async (req, res) => {
   if (!userId || !isValidObjectId(userId)) {
     throw new ApiError(400, "Invalid user id");
   }
-
+  // Get channel by userId
+  // get total number of subscripbers and likes
+  const video = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "onVideo",
+        as: "totalLikes",
+      },
+    },
+    {
+      $group: {
+        _id: userId,
+        totalVideos: { $count: {} },
+        totalViews: { $sum: "$views" },
+        totalLikes :{ $sum:{ $size: "$totalLikes"}}
+      },
+    },
+  ]);
+  // validation
+  if (!video) {
+    throw new ApiError(404, "No videos found for this channel");
+  }
+  // return
+  res.status(200).json(
+    new ApiResponse(200, "Channel stats fetched successfully", {
+      videoDetails: video,
+    })
+  );
 });
-
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   const userId = req.user?._id;
   const { limit = 10, page = 1 } = req.query;
   // Validate userId and channelId
-  if (!userId ||!isValidObjectId(userId)) {
+  if (!userId || !isValidObjectId(userId)) {
     throw new ApiError(400, "Invalid user id");
   }
 
   // Get videos by channel owner
   const videos = await Video.find({ owner: userId })
-   .sort({ createdAt: -1 })
-   .limit(Number(limit))
-   .skip(Number(page) * Number(limit) - Number(limit));
+    .sort({ createdAt: -1 })
+    .limit(Number(limit))
+    .skip(Number(page) * Number(limit) - Number(limit));
 
-    // validation 
-    if (!videos) {
+  // validation
+  if (!videos) {
     throw new ApiError(404, "No videos found for this channel");
   }
   // response
-  res.status(200).json(new ApiResponse(200, "Videos fetched successfully", videos));
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Videos fetched successfully", videos));
 });
-
-
 
 export { getChannelStats, getChannelVideos };
